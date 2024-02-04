ce0bb50a3e997eb7617bbb43dcd2f3f704389917
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-02-04 15:36:20 -0300

    create ws simple server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log('Hola mundo')
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+
+  console.log('Client connected');
+
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('Hola desde el servidor');
+});
