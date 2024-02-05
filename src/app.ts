42109227e270a5c2c3187292f2a57019ca54faec
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-02-05 10:09:32 -0300

    server broadcast

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer,WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
@@ -9,8 +9,21 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
 
-  ws.send('Hola desde el servidor');
+    const payload = JSON.stringify({
+      type: 'custom-message',
+      payload: data.toString()
+    })
+
+    // ws.send(JSON.stringify(payload));
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(payload);
+      }
+    });
+
+    ws.on('close', function close() {
+      console.log('Client disconnected')
+    })
+  })
 });
